68df6d914dde557300398147190ab5f29b8cdd6b
ARSHamirali <arshamirali604@gmail.com>
2024-10-03 14:27:24 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+// var number=+prompt('Enter any number')
+// console.log(Math.round(number))
+// console.log(Math.ceil(number))
+// console.log(Math.floor(number))
+
+
+// var number=+prompt('Enter any Negavtive number')
+// console.log(Math.round(number))
+// console.log(Math.ceil(number))
+// console.log(Math.floor(number))
+
+// console.log(Math.round(-20.45))
+
+// console.log(Math.round(Math.random()*6))+1
+
+// console.log(Math.round(Math.random(1)*2))
+
+
+
